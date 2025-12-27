abb48b29af2e845c836fceac06e111e4c8917185
rishita-pixie <nainwalrishita87@gmail.com>
2025-12-27 17:02:13 +0530

    modified the ddoc.js for delete and ccreate

--- backend/routes/docs.js
+++ backend/routes/docs.js
@@ -2,18 +2,17 @@ import express from "express";
 import Document from "../models/Document.js";
 import authMiddleware from "../middleware/auth.js";
 
-
 const router = express.Router();
 
 // CREATE DOCUMENT
-router.post("/",authMiddleware, async (req, res) => {
+router.post("/", authMiddleware, async (req, res) => {
   try {
     const { title, content } = req.body;
 
     const newDoc = new Document({
       title,
       content,
-       owner: req.user.id
+      owner: req.user.id
     });
 
     const savedDoc = await newDoc.save();
@@ -23,11 +22,23 @@ router.post("/",authMiddleware, async (req, res) => {
   }
 });
 
+// GET ALL DOCUMENTS (only logged-in user)
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const docs = await Document.find({ owner: req.user.id });
+    res.json(docs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // GET DOCUMENT BY ID
-router.get("/:id", authMiddleware,async (req, res) => {
+router.get("/:id", authMiddleware, async (req, res) => {
   try {
-    const doc = await Document.findById(req.params.id);
+    const doc = await Document.findOne({
+      _id: req.params.id,
+      owner: req.user.id
+    });
 
     if (!doc) {
       return res.status(404).json({ message: "Document not found" });
@@ -39,16 +50,43 @@ router.get("/:id", authMiddleware,async (req, res) => {
   }
 });
 
-// GET ALL DOCUMENTS
-router.get("/", authMiddleware, async (req, res) => {
+// UPDATE DOCUMENT
+router.put("/:id", authMiddleware, async (req, res) => {
   try {
-    const docs = await Document.find();
-    res.json(docs);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    const { title, content } = req.body;
+
+    const updated = await Document.findOneAndUpdate(
+      { _id: req.params.id, owner: req.user.id },
+      { title, content },
+      { new: true }
+    );
+
+    if (!updated) {
+      return res.status(404).json({ message: "Document not found" });
+    }
+
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
   }
 });
 
+// DELETE DOCUMENT
+router.delete("/:id", authMiddleware, async (req, res) => {
+  try {
+    const deleted = await Document.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user.id
+    });
 
+    if (!deleted) {
+      return res.status(404).json({ message: "Document not found" });
+    }
+
+    res.json({ message: "Document deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 export default router;
